d12536eca4b391eb948c7ffc1e99daa6249db7f1
ErgonomicQ <ergonomicQ@gmail.com>
2023-07-20 17:23:08 -0700

    Reorganized server.js file. HTML route came before API's resulting in false routing.

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'Develop', 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+// API routes
+app.post('/api/savednotes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4(); // This will assign a new and unique id to the post
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'Develop', 'db', 'db.json'), 'utf8'));
+  notes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, 'Develop', 'db', 'db.json'), JSON.stringify(notes));
+  res.json(newNote);
+});
+
+app.get('/api/savednotes', (req, res) => {
+  const filePath = path.join(__dirname, 'Develop', 'db', 'db.json');
+  console.log(filePath); // Log the path to check if it's correct
+  const notes = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  res.json(notes);
+});
 
 // Routing to HTML
 app.get('/notes', (req, res) => {
@@ -18,21 +36,6 @@ app.get('/notes', (req, res) => {
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'Develop', 'public', 'index.html'));
-});
-
-// API routes
-app.get('/api/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'Develop', 'db', 'db.json'), 'utf8'));
-  res.json(notes);
-});
-
-app.post('/api/notes', (req, res) => {
-  const newNote = req.body;
-  newNote.id = uuidv4(); // This will assign a new and unique id to the post
-  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'Develop', 'db', 'db.json'), 'utf8'));
-  notes.push(newNote);
-  fs.writeFileSync(path.join(__dirname, 'Develop', 'db', 'db.json'), JSON.stringify(notes));
-  res.json(newNote);
 });
 
 // Server start
